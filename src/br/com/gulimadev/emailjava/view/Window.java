c8fe1c6b6240b5a25cb72e3787705759b2030384
gulimadev <96209984+gulimadev@users.noreply.github.com>
2022-01-24 20:13:30 -0300

    Add tela de login e main

--- src/br/com/gulimadev/emailjava/view/Window.java
+++ src/br/com/gulimadev/emailjava/view/Window.java
@@ -1,26 +1,50 @@
 package br.com.gulimadev.emailjava.view;
 
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.JTextField;
 import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import javax.swing.JDesktopPane;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.UIManager;
 
 public class Window extends JPanel {
+	private JTextField email;
+	private JPasswordField senha;
 
 	/**
 	 * Create the panel.
 	 */
 	public Window() {
 		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Email Java");
+		lblNewLabel.setFont(UIManager.getFont("ColorChooser.font"));
+		lblNewLabel.setBounds(118, 11, 122, 14);
+		add(lblNewLabel);
+		
+		email = new JTextField();
+		email.setBounds(34, 46, 239, 20);
+		add(email);
+		email.setColumns(10);
+		
+		senha = new JPasswordField();
+		senha.setBounds(34, 94, 239, 20);
+		add(senha);
+		
+		JLabel lblNewLabel_1 = new JLabel("Email:");
+		lblNewLabel_1.setBounds(36, 28, 63, 14);
+		add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Senha:");
+		lblNewLabel_1_1.setBounds(34, 77, 63, 14);
+		add(lblNewLabel_1_1);
+		
+		JButton entrar = new JButton("Entrar");
+		entrar.setBounds(103, 137, 89, 23);
+		add(entrar);
 		setVisible(true);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(290, 5, 10, 10);
-		add(panel);
-		
-		JDesktopPane desktopPane = new JDesktopPane();
-		desktopPane.setBounds(10, 5, 1, 1);
-		add(desktopPane);
 
 	}
 }
